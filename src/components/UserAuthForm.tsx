d24303b2b5ec47390d199845b5c756859866a5e2
Michaux Kelley <michauxkelley@gmail.com>
2023-08-18 15:23:02 -0400

    Remove error from user auth form

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -16,7 +16,6 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
     setIsLoading(true);
 
     try {
-      throw new Error();
       await signIn('google');
     } catch (error) {
       toast({
