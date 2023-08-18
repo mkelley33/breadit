9da2799213c75cd82d4801eb54585befdf707c0f
Michaux Kelley <michauxkelley@gmail.com>
2023-08-18 15:21:39 -0400

    Add shadcdn toast notification to user auth form

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,13 +1,44 @@
+'use client';
+
 import { cn } from '@/lib/utils';
 import { Button } from './ui/Button';
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import { signIn } from 'next-auth/react';
+import { Icons } from './Icons';
+import { useToast } from '@/hooks/use-toast';
 
 interface UserAuthFormProps extends React.HtmlHTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useToast();
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+      throw new Error();
+      await signIn('google');
+    } catch (error) {
+      toast({
+        title: 'There was a problem',
+        description: 'There was an error logging in with Google',
+        variant: 'destructive',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className={cn('flex justify-center', className)} {...props}>
-      <Button>Google</Button>
+      <Button
+        onClick={loginWithGoogle}
+        isLoading={isLoading}
+        size="sm"
+        className="w-full"
+      >
+        {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />} Google
+      </Button>
     </div>
   );
 };
