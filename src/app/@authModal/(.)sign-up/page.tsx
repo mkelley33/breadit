a357ca068734be0133def93f4e8ec7b81d69d886
Michaux Kelley <michauxkelley@gmail.com>
2023-08-19 16:02:03 -0400

    Intercept signup with modal

--- src/app/@authModal/(.)sign-up/page.tsx
+++ src/app/@authModal/(.)sign-up/page.tsx
@@ -1,5 +1,5 @@
 import CloseModal from '@/components/CloseModal';
-import Signin from '@/components/SignIn';
+import SignUp from '@/components/SignUp';
 
 const page = () => {
   return (
@@ -9,7 +9,7 @@ const page = () => {
           <div className="absolute top-4 right-4">
             <CloseModal />
           </div>
-          <Signin />
+          <SignUp />
         </div>
       </div>
     </div>
