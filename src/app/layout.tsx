7ca04ef5958db7125f7caaaa7728fedf0313d0c6
Michaux Kelley <michauxkelley@gmail.com>
2023-08-20 12:34:23 -0400

    Add create subreddit form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar';
+import Providers from '@/components/Providers';
 import { Toaster } from '@/components/ui/Toaster';
 import { cn } from '@/lib/utils';
 import '@/styles/globals.css';
@@ -27,12 +28,14 @@ export default function RootLayout({
       )}
     >
       <body className="max-h-screen pt-12 bg-slate-50 antialiased">
-        {/* @ts-expect-error server component */}
-        <Navbar />
-        {authModal}
-        <main className="container max-w-7xl mx-auto h-full pt-12">
-          {children}
-        </main>
+        <Providers>
+          {/* @ts-expect-error server component */}
+          <Navbar />
+          {authModal}
+          <main className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+          </main>
+        </Providers>
         <Toaster />
       </body>
     </html>
