9f90ec077b34f35f714fdcdb775666a6d8d00ce3
Michaux Kelley <michauxkelley@gmail.com>
2023-08-20 23:11:53 -0400

    Add ability to create a post

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -7,6 +7,10 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import type EditorJS from '@editorjs/editorjs';
 import { uploadFiles } from '@/lib/uploadthing';
+import { toast } from '@/hooks/use-toast';
+import axios from 'axios';
+import { useMutation } from '@tanstack/react-query';
+import { usePathname, useRouter } from 'next/navigation';
 
 const Editor = ({ subredditId }: { subredditId: string }) => {
   const {
@@ -22,14 +26,11 @@ const Editor = ({ subredditId }: { subredditId: string }) => {
     },
   });
 
-  const ref = useRef<EditorJS | null>(null);
+  const ref = useRef<EditorJS | null | undefined>(null);
   const [isMounted, setIsMounted] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setIsMounted(true);
-    }
-  }, []);
+  const _titleRef = useRef<HTMLTextAreaElement>(null);
+  const pathname = usePathname();
+  const router = useRouter();
 
   const initializeEditor = useCallback(async () => {
     const EditorJS = (await import('@editorjs/editorjs')).default;
@@ -82,26 +83,101 @@ const Editor = ({ subredditId }: { subredditId: string }) => {
   }, []);
 
   useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsMounted(true);
+    }
+  }, []);
+
+  useEffect(() => {
     const init = async () => {
       await initializeEditor();
 
       setTimeout(() => {
-        // set focus to the title
-      });
+        _titleRef.current?.focus();
+      }, 0);
     };
 
     if (isMounted) {
       init();
 
-      return () => {};
+      return () => {
+        ref.current?.destroy();
+        ref.current = undefined;
+      };
     }
   }, [isMounted, initializeEditor]);
 
+  const { mutate: createPost } = useMutation({
+    mutationFn: async ({
+      title,
+      content,
+      subredditId,
+    }: PostCreationRequest) => {
+      const payload: PostCreationRequest = {
+        subredditId,
+        title,
+        content,
+      };
+      const { data } = await axios.post('/api/subreddit/post/create', payload);
+      return data;
+    },
+    onError: () => {
+      return toast({
+        title: 'Something went wrong',
+        description: 'Your post was not published, please try again later.',
+        variant: 'destructive',
+      });
+    },
+    onSuccess: () => {
+      // r/mycommunity/submit into r/mycommunity
+      const newPathname = pathname.split('/').slice(0, -1).join('/');
+      router.push(newPathname);
+      router.refresh();
+      return toast({
+        description: 'Your post has been published.',
+      });
+    },
+  });
+
+  async function onSubmit(data: PostCreationRequest) {
+    const blocks = await ref.current?.save();
+    const payload: PostCreationRequest = {
+      title: data.title,
+      content: blocks,
+      subredditId,
+    };
+    createPost(payload);
+  }
+
+  useEffect(() => {
+    if (Object.keys(errors).length) {
+      for (const [_key, value] of Object.entries(errors)) {
+        toast({
+          title: 'Something went wrong',
+          description: (value as { message: string }).message,
+          variant: 'destructive',
+        });
+      }
+    }
+  }, [errors]);
+
+  const { ref: titleRef, ...rest } = register('title');
+
   return (
     <div className="w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200">
-      <form id="subreddit-post-form" className="w-fit" onSubmit={() => {}}>
+      <form
+        id="subreddit-post-form"
+        className="w-fit"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className="prose prose-stone dark:prose-invert">
           <TextareaAutosize
+            ref={(e) => {
+              titleRef(e);
+              // @ts-ignore
+              _titleRef.current = e;
+            }}
+            {...rest}
             placeholder="Title"
             className="w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none"
           />
